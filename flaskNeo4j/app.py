94887b59af9a765c7b395bda1b2468e0b1e5ffb2
abartczak629 <a.bartczak.629@studms.ug.edu.pl>
2023-12-03 21:26:08 +0100

    changed functions so arguments are passed in tx.run()

--- flaskNeo4j/app.py
+++ flaskNeo4j/app.py
@@ -51,24 +51,30 @@ def get_employees_route():
 
 
 def is_employee_unique(tx, name, surname):
-    query = f"MATCH (e:Employee {{name: '{name}', surname: '{surname}'}}) RETURN COUNT(e) as count"
-    result = tx.run(query).single()
+    query = "MATCH (e:Employee {name: $name, surname: $surname}) RETURN COUNT(e) as count"
+    result = tx.run(query, name=name, surname=surname).single()
     return result["count"] == 0
 
 
 def degrade_prev_manager(tx, department):
-    query = f"MATCH (e:Employee {{department: '{department}', position: 'Manager'}})-[rel: MANAGES]->(d: Department) SET e.position = 'Employee' DELETE rel"
+    query = (
+        "MATCH (e:Employee {department: $department, position: 'Manager'})"
+        "-[rel: MANAGES]->(d: Department) SET e.position = 'Employee' DELETE rel"
+    )
     tx.run(query, department=department)
 
 
 def add_employee(tx, name, surname, position, department):
-    query = f"MATCH (d: Department {{name: '{department}'}}) "
-    query += f"CREATE (e:Employee {{name: '{name}', surname: '{surname}', position: '{position}', department: '{department}'}}) "
-    query += f"CREATE (e)-[:WORKS_IN]->(d)"
+    query = (
+        "MATCH (d: Department {name: $department}) "
+        "CREATE (e:Employee {name: $name, surname: $surname, "
+        "position: $position, department: $department}) "
+        "CREATE (e)-[:WORKS_IN]->(d)"
+    )
 
     if position == "Manager":
         degrade_prev_manager(tx, department)
-        query += f"CREATE (e)-[:MANAGES]->(d)"
+        query += "CREATE (e)-[:MANAGES]->(d)"
 
     tx.run(query, name=name, surname=surname, position=position, department=department)
 
@@ -93,22 +99,22 @@ def add_employee_route():
         if position == "Manager":
             session.write_transaction(degrade_prev_manager, department)
         session.write_transaction(add_employee, name, surname, position, department)
-
     return jsonify({'message': 'Employee added successfully'}), 201
 
+
 def exists_employee(tx, employee_id):
-    query = f"MATCH (e:Employee) WHERE ID(e) = {employee_id} RETURN COUNT(e) as count"
-    result = tx.run(query).single()
-    if result["count"] == 0:
-        return False
-    return True
+    query = "MATCH (e:Employee) WHERE ID(e) = $employee_id RETURN COUNT(e) as count"
+    result = tx.run(query, employee_id=employee_id).single()
+    return result["count"] > 0
+
 
 def is_a_manager(tx, employee_id):
-    query = f"MATCH (e:Employee)-[:MANAGES]->(d:Department) WHERE ID(e) = {employee_id} RETURN COUNT(e) as count"
-    result = tx.run(query).single()
-    if result["count"] == 0:
-        return False
-    return True
+    query = (
+        "MATCH (e:Employee)-[:MANAGES]->(d:Department) "
+        "WHERE ID(e) = $employee_id RETURN COUNT(e) as count"
+    )
+    result = tx.run(query, employee_id=employee_id).single()
+    return result["count"] > 0
 
 
 @app.route('/employees/<int:id>', methods=['DELETE'])
@@ -121,107 +127,60 @@ def delete_employee_route(id):
     else:
         return jsonify({'message': result}), 200
 
+
 def delete_employee(tx, employee_id):
     if not exists_employee(tx, employee_id):
         return "not_found"
     
     if is_a_manager(tx, employee_id):
-        delete_query = f"MATCH (m:Employee)-[:WORKS_IN]->(d:Department) WHERE ID(m) = {employee_id} DETACH DELETE m RETURN d.name"
-        result = tx.run(delete_query).single()
+        delete_query = (
+            "MATCH (m:Employee)-[:WORKS_IN]->(d:Department) "
+            "WHERE ID(m) = $employee_id DETACH DELETE m RETURN d.name"
+        )
+        result = tx.run(delete_query, employee_id=employee_id).single()
         dep_name = result['d.name']
         mesg = handle_manager_deletion(tx, dep_name)
         return mesg
-    
     else:
-        delete_query = f"MATCH (e:Employee) WHERE ID(e) = {employee_id} DETACH DELETE e"
-        tx.run(delete_query)
+        delete_query = "MATCH (e:Employee) WHERE ID(e) = $employee_id DETACH DELETE e"
+        tx.run(delete_query, employee_id=employee_id)
         return "deleted employee successfully"
 
+
 def handle_manager_deletion(tx, dep_name):
-    query_number_of_employees = f"MATCH (e:Employee)-[:WORKS_IN]->(d:Department {{name: '{dep_name}'}}) RETURN COUNT(e) AS number_of_employees"
-    result = tx.run(query_number_of_employees).single()
+    query_number_of_employees = (
+        "MATCH (e:Employee)-[:WORKS_IN]->(d:Department {name: $dep_name}) "
+        "RETURN COUNT(e) AS number_of_employees"
+    )
+    result = tx.run(query_number_of_employees, dep_name=dep_name).single()
 
     if result["number_of_employees"] >= 1:
-        query_new_manager = f"MATCH (e:Employee)-[:WORKS_IN]->(d:Department {{name: '{dep_name}'}}) WITH e, d LIMIT 1 SET e.position = 'Manager' CREATE (e)-[:MANAGES]->(d) RETURN e"
-        tx.run(query_new_manager)
+        query_new_manager = (
+            "MATCH (e:Employee)-[:WORKS_IN]->(d:Department {name: $dep_name}) "
+            "WITH e, d LIMIT 1 SET e.position = 'Manager' CREATE (e)-[:MANAGES]->(d) RETURN e"
+        )
+        tx.run(query_new_manager, dep_name=dep_name)
         return "set new manager"
     else:
-        query_delete_dep = f"MATCH (d: Department {{name: '{dep_name}'}}) DELETE d"
-        tx.run(query_delete_dep)
+        query_delete_dep = "MATCH (d: Department {name: $dep_name}) DELETE d"
+        tx.run(query_delete_dep, dep_name=dep_name)
         return "deleted department"
 
 
-def edit_employee(tx, employee_id, data):
-    # Pobierz aktualne dane pracownika
-    current_data_query = f"MATCH (e:Employee) WHERE ID(e) = {employee_id} RETURN e"
-    current_data = tx.run(current_data_query).single()
-
-    if not current_data:
-        return "Employee not found"
-
-    # Przygotuj zapytanie edycji pracownika
-    edit_query = "MATCH (e:Employee) WHERE ID(e) = $employee_id SET "
-    parameters = {}
-
-    # Sprawdź, czy dane są dostarczone, a następnie dodaj do zapytania edycji
-    if 'name' in data:
-        edit_query += "e.name = $name, "
-        parameters['name'] = data['name']
-
-    if 'surname' in data:
-        edit_query += "e.surname = $surname, "
-        parameters['surname'] = data['surname']
-
-    if 'position' in data:
-        edit_query += "e.position = $position, "
-        parameters['position'] = data['position']
-
-    if 'department' in data:
-        # Sprawdź, czy departament istnieje
-        # if not exists_department(tx, data['department_id']):
-        #     return "Department not found"
-
-        edit_query += "WITH e MATCH (d:Department) WHERE ID(d) = $department MERGE (e)-[:WORKS_IN]->(d) "
-        parameters['department_id'] = data['department']
-
-    # Usuń ostatnią spację i przecinek z zapytania
-    edit_query = edit_query.rstrip(', ')
-
-    # Uruchom zapytanie edycji pracownika
-    tx.run(edit_query, employee_id=employee_id, **parameters)
-
-    return "success"
-
-
-
-@app.route('/employees/<int:id>', methods=['PUT'])
-def edit_employee_route(id):
-    with driver.session() as session:
-        if not session.read_transaction(exists_employee, id):
-            return jsonify({'error': 'employee not found'}), 404
-    
-        data = request.get_json()
-
-        if not data:
-            return jsonify({'error': 'no data provided for update'}), 400
-        
-        result = session.write_transaction(edit_employee, id, data)
-        
-        if result == "success":
-            return jsonify({'message': 'Employee updated successfully'}), 200
-        else:
-            return jsonify({'error': result}), 500       
-
-
-def get_subordinates(tx, id):
-    if exists_employee(tx, id) and is_a_manager(tx, id):
-        query = f"MATCH (m:Employee)-[:MANAGES]->(d:Department) WHERE ID(m) = {id} MATCH (e:Employee)-[:WORKS_IN]->(d) WHERE e.position <> 'Manager' RETURN e;"
-        results = tx.run(query).data()
+def get_subordinates(tx, employee_id):
+    if exists_employee(tx, employee_id) and is_a_manager(tx, employee_id):
+        query = (
+            "MATCH (m:Employee)-[:MANAGES]->(d:Department) "
+            "WHERE ID(m) = $employee_id "
+            "MATCH (e:Employee)-[:WORKS_IN]->(d) "
+            "WHERE e.position <> 'Manager' RETURN e"
+        )
+        results = tx.run(query, employee_id=employee_id).data()
         subordinates = [{'name': result['e']['name'], 
-                     'surname': result['e']['surname'], 
-                     'position': result['e']['position'],
-                     'department': result['e']['department']} 
-                    for result in results]
+                         'surname': result['e']['surname'], 
+                         'position': result['e']['position'],
+                         'department': result['e']['department']} 
+                        for result in results]
         return subordinates
     return None
 
@@ -232,21 +191,30 @@ def get_subordinates_route(id):
         subordinates = session.read_transaction(get_subordinates, id)
 
         if subordinates is None:
-            return jsonify({'error': 'Employee not found'}), 404
+            return jsonify({'error': 'Employee not found or is not a manager'}), 404
 
     response = {'subordinates': subordinates}
     return jsonify(response)
 
 
 def get_department_info(tx, employee_id=None):
-    query_dep_name = f"MATCH (e: Employee)-[:WORKS_IN]->(d: Department) WHERE ID(e) = {employee_id} RETURN d.name as name"
-    dep_name = tx.run(query_dep_name).single()['name']
+    query_dep_name = (
+        "MATCH (e: Employee)-[:WORKS_IN]->(d: Department) "
+        "WHERE ID(e) = $employee_id RETURN d.name as name"
+    )
+    dep_name = tx.run(query_dep_name, employee_id=employee_id).single()['name']
 
-    query_employees_number = f"MATCH (e: Employee)-[]->(d: Department) WHERE d.name = '{dep_name}' RETURN COUNT(e) as numberOfEmployees"
-    employees_number = tx.run(query_employees_number).single()["numberOfEmployees"]
+    query_employees_number = (
+        "MATCH (e: Employee)-[:WORKS_IN]->(d: Department {name: $dep_name}) "
+        "RETURN COUNT(e) as numberOfEmployees"
+    )
+    employees_number = tx.run(query_employees_number, dep_name=dep_name).single()["numberOfEmployees"]
 
-    query_manager = f"MATCH (m: Employee)-[:MANAGES]->(d: Department) WHERE d.name = '{dep_name}' RETURN m"
-    manager = tx.run(query_manager).data()[0]['m']
+    query_manager = (
+        "MATCH (m: Employee)-[:MANAGES]->(d: Department {name: $dep_name}) "
+        "RETURN m"
+    )
+    manager = tx.run(query_manager, dep_name=dep_name).data()[0]['m']
 
     info = {'name': dep_name, 'numberOfEmployees': employees_number, 'manager': manager}
     return info
@@ -256,13 +224,12 @@ def get_department_info(tx, employee_id=None):
 def get_employees_department_info_route(id):
     with driver.session() as session:
         employee = session.read_transaction(exists_employee, id)
-        
-        if employee is None:
+
+        if not employee:
             return jsonify({'error': 'Employee not found'}), 404
         else:
             response = session.read_transaction(get_department_info, employee_id=id)
             return jsonify({'info': response}), 200
-
 
 
 def get_all_departments(tx, filters=None, sort_by=None):
@@ -300,7 +267,7 @@ def get_all_departments_route():
         departments = session.read_transaction(get_all_departments, filters=filters, sort_by=sort_by)
 
     return jsonify({'info': departments}), 200
-        
+
 
 @app.route('/departments/<int:department_id>/employees', methods=['GET'])
 def get_department_employees_route(department_id):
